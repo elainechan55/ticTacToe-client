8a5a019b1d2638486fce5d8b62ae352b51401ce0
elainechan55 <emchan55@gmail.com>
2021-04-02 12:36:10 -0500

    Add Game Curl-Scripts Folder
    Create new-game curl shell script (POST)

---
+++ curl-scripts/game/new-game.sh
@@ -0,0 +1,9 @@
+# VARIABLE=VALUE sh curl-scripts/game/new-game.sh
+
+curl "https://tic-tac-toe-api-development.herokuapp.com/games" \
+  --include \
+  --request POST \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --header "Content-Type: application/json" \
+
+echo
