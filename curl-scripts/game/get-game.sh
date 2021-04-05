d27637ab5ee4c88aa5e26276f19ebc6a2ec2198a
elainechan55 <emchan55@gmail.com>
2021-04-05 08:14:14 -0500

    Implement and Test get-game Curl Script

---
+++ curl-scripts/game/get-game.sh
@@ -0,0 +1,12 @@
+# 606a7de39528140017acb6a3 - gameid
+# 5ecfd6aacb0bf22167a4d889b6c81bf6 -token
+
+# VARIABLE=VALUE sh curl-scripts/game/sign-up.sh
+
+curl "https://tic-tac-toe-api-development.herokuapp.com/games/${ID}" \
+  --include \
+  --request GET \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --header "Content-Type: application/json" \
+
+echo
