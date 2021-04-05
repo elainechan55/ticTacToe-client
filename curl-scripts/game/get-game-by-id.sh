0c5a0aca26c56fceabdd5f36a306f630e99a6f40
elainechan55 <emchan55@gmail.com>
2021-04-05 08:52:09 -0500

    Rename File for Clarification

--- curl-scripts/game/get-game-by-id.sh
+++ curl-scripts/game/get-game-by-id.sh
@@ -1,7 +1,7 @@
 # 606a7de39528140017acb6a3 - gameid
 # 5ecfd6aacb0bf22167a4d889b6c81bf6 -token
 
-# VARIABLE=VALUE sh curl-scripts/game/sign-up.sh
+# VARIABLE=VALUE sh curl-scripts/game/get-game.sh
 
 curl "https://tic-tac-toe-api-development.herokuapp.com/games/${ID}" \
   --include \
